18d2acbdbdacc61dff2bcb6fbd7cf10ff19729a7
sebasare <61071641+sebasare@users.noreply.github.com>
2020-03-06 21:15:08 -0500

    evaluacionWEB1
    EJERCICIOS 8-9

---
+++ funciones.php
@@ -0,0 +1,30 @@
+<?php
+//ejercicio 8
+ function calcularEdad($nacimiento){
+    $edad=(2020-$nacimiento);
+    echo("si nacio en 1991 usted tiene : ".$edad);
+    
+    
+
+}
+function calcularEdad2($nacimiento){
+    $edad=(2020-$nacimiento);
+    echo("si nacio en 1995 usted tiene : ".$edad);
+}
+
+function calcularEdad3($nacimiento){
+    $edad=(2020-$nacimiento);
+    echo("si nacio en 1987 usted tiene : ".$edad);
+}
+    
+
+//ejercicio 9
+function calcularBiotipo($estatura){
+    $biotipo=($estatura="Biotipo BAJO");
+    echo("para una altura de 1.65 su biotipo es: ".$estatura);
+}
+    
+
+    
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//ejercicio 8
+include('funciones.php');
+
+calcularEdad(1991);
+echo"<br>..................................<br>";
+calcularEdad2(1995);
+echo"<br>..................................<br>";
+calcularEdad3(1987);
+echo"<br>..................................<br>";
+
+ //ejercicio 9
+ calcularBiotipo("bajo");
+ echo"<br>..................................<br>"
+
+
+ 
+
+
+
+
+
+
+
+?>
